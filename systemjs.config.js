5de6480d11c5cc72257f11df507ffa928ca0daea
alexzuza <alexzuza@yandex.ru>
2016-05-13 22:36:32 +0400

    Add systemjs.config

---
+++ systemjs.config.js
@@ -0,0 +1,78 @@
+(function(global) {
+
+  // map tells the System loader where to look for things
+  var map = {
+    'app':                        'app',
+    'rxjs':                       'node_modules/rxjs',
+    'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+    '@angular':                   'node_modules/@angular',
+    '@angular2-material':         'node_modules/@angular2-material'
+  };
+
+  // packages tells the System loader how to load when no filename and/or no extension
+  var packages = {
+    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'rxjs':                       { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    '@angular2-material/core': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'core.js'
+    },
+    '@angular2-material/checkbox': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'checkbox.js'
+    },
+
+    '@angular2-material/button': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'button.js'
+    },
+    '@angular2-material/toolbar': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'toolbar.js'
+    },
+    '@angular2-material/card': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'card.js'
+    },
+    '@angular2-material/input': {
+      format: 'cjs',
+      defaultExtension: 'js',
+      main: 'input.js'
+    }
+  };
+
+  var packageNames = [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/router',
+    '@angular/router-deprecated',
+    '@angular/testing',
+    '@angular/upgrade',
+  ];
+
+  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
+  packageNames.forEach(function(pkgName) {
+    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
+
+  var config = {
+    map: map,
+    packages: packages
+  }
+
+  // filterSystemConfig - index.html's chance to modify config before we register it.
+  if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+
+  System.config(config);
+
+})(this);
