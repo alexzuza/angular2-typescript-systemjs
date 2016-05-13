367418bb9cf6bfd811771aac0ce025c2f3b97c18
alexzuza <alexzuza@yandex.ru>
2016-05-13 22:40:03 +0400

    remove unused packages

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,46 +5,14 @@
     'app':                        'app',
     'rxjs':                       'node_modules/rxjs',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    '@angular':                   'node_modules/@angular',
-    '@angular2-material':         'node_modules/@angular2-material'
+    '@angular':                   'node_modules/@angular'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
-    'angular2-in-memory-web-api': { defaultExtension: 'js' },
-    '@angular2-material/core': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'core.js'
-    },
-    '@angular2-material/checkbox': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'checkbox.js'
-    },
-
-    '@angular2-material/button': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'button.js'
-    },
-    '@angular2-material/toolbar': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'toolbar.js'
-    },
-    '@angular2-material/card': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'card.js'
-    },
-    '@angular2-material/input': {
-      format: 'cjs',
-      defaultExtension: 'js',
-      main: 'input.js'
-    }
+    'angular2-in-memory-web-api': { defaultExtension: 'js' }
   };
 
   var packageNames = [
