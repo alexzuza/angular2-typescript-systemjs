477fd44abad32e808354b8448e1d54e9e15fbab4
alexzuza <alexzuza@yandex.ru>
2016-09-18 17:26:57 +0400

    add angular2 material 2.0.0-alpha.8-2

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Angular 2 Systemjs start</h1>`
+  template: `<h1>Angular 2 Systemjs start</h1>
+  
+  <button md-button>FLAT</button>
+<button md-raised-button>RAISED</button>
+  <button md-button-raised>Button</button>
+  `
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { MdButtonModule} from '@angular2-material/button'
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MdButtonModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
