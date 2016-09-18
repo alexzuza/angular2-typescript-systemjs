a5cf4a3c353168e29dcdb7ae699575d8e3f2f0b3
alexzuza <alexzuza@yandex.ru>
2016-09-18 17:41:25 +0400

    remove unused code

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { MdButtonModule} from '@angular2-material/button'
 
 @NgModule({
-  imports:      [ BrowserModule, MdButtonModule ],
+  imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
--- systemjs.config.js
+++ systemjs.config.js
@@ -3,42 +3,79 @@
  * Adjust as necessary for your application needs.
  */
 (function (global) {
-  System.config({
+  function mapIndex() {
+    return {
+      'app': 'app',
+      '@angular': 'npm:@angular',
+      'rxjs': 'npm:rxjs',
+      'typescript': 'npm:typescript/lib/typescript.js' //add typescript map
+    };
+  }
+
+  function mapUmd() {
+    var map = {
+      'app': 'app',
+      'rxjs': 'npm:rxjs',
+    };
+
+    [ 'core',
+      'common',
+      'compiler',
+      'forms',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'router'
+    ].forEach(function (name) {
+      map['@angular/' + name] = 'npm:@angular/' + name + '/bundles/' + name + '.umd.js';
+    });
+
+    return map;
+  }
+
+  System.packageWithIndex = 1;
+
+  var packages = {
+    app: {
+      main: './main.js',
+      defaultExtension: 'js'
+    },
+    rxjs: {
+      defaultExtension: 'js'
+    },
+    'angular2-in-memory-web-api': {
+      main: './index.js',
+      defaultExtension: 'js'
+    }
+  };
+
+  if (System.packageWithIndex) {
+    [ 'core',
+      'common',
+      'compiler',
+      'forms',
+      'http',
+      'platform-browser',
+      'platform-browser-dynamic',
+      'router'
+    ].forEach(function (name) {
+      packages['@angular/' + name] = { main: 'index.js' };
+    });
+  }
+
+  var config = {
+    // paths serve as alias
     paths: {
-      // paths serve as alias
       'npm:': 'node_modules/'
     },
-    // map tells the System loader where to look for things
-    map: {
-      // our app is within the app folder
-      app: 'app',
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      
-      // other libraries
-      'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
-    },
+    map: System.packageWithIndex ? mapIndex() : mapUmd(),
     // packages tells the System loader how to load when no filename and/or no extension
-    packages: {
-      app: {
-        main: './main.js',
-        defaultExtension: 'js'
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      },
-      'angular2-in-memory-web-api': {
-        main: './index.js',
-        defaultExtension: 'js'
-      }
-    }
-  });
+    packages: packages
+  };
+
+  if (System.packageWithIndex) {
+    config['transpiler'] = 'typescript';
+  }
+
+  System.config(config);
 })(this);
