01826e098017d113f1378e17d48d9eb9d1f9cfc2
alexzuza <alexzuza@yandex.ru>
2017-05-10 00:35:55 +0400

    Initial commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { UpgradeModule } from '@angular/upgrade/static';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, UpgradeModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  entryComponents:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() {}
+}
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { UpgradeModule } from '@angular/upgrade/static';
 import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { downgradeComponent } from '@angular/upgrade/static';
+
+declare var angular: angular.IAngularStatic;
+let myApp = angular.module('mainApp', [])
+  .directive(
+    'appComponent',
+    downgradeComponent({component: AppComponent}) as angular.IDirectiveFactory
+  );
+myApp.controller('MainCtrl', function() {
+  this.message = 'Upgrade angularjs to angular'
+});
+
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
+  const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
+  upgrade.bootstrap(document.documentElement, ['mainApp']);
+});
