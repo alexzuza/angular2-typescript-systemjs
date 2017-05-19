9f4b755694240608d51751be0e32481db0539652
alexzuza <alexzuza@yandex.ru>
2017-05-19 10:03:32 +0400

    Temporary comment aot

--- app/main-aot.ts
+++ app/main-aot.ts
@@ -1,5 +1,5 @@
-import { platformBrowser } from '@angular/platform-browser';
+/*import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
 
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);*/
