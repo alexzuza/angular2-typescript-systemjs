ecc629c0b7f8d598ce63f2f4a7abb3a199e7b6b8
alexzuza <alexzuza@yandex.ru>
2017-06-13 08:18:10 +0400

    Update to 4.2.2

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  declarations: [
+    AppComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
