d5102028bd31ca864fc9a9a008352f6a830a8e13
Alexey Zuev <alexzuza@yandex.ru>
2017-08-11 18:10:07 +0400

    Map to cdk

--- systemjs.config.js
+++ systemjs.config.js
@@ -26,7 +26,7 @@
       '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
       '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
       '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
-
+      '@angular/cdk': 'npm:@angular/cdk/bundles/cdk.umd.js',
       // other libraries
       'rxjs': 'npm:rxjs'
     },
