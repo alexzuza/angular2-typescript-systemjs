2b9fbe3059f0c3583fda89021f38b50260451506
alexzuza <alexzuza@yandex.ru>
2017-10-02 23:05:00 +0400

    Remove unused imports

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-/*import { MdButtonModule, MdOptionModule, MdSelectModule } from '@angular/material'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';*/
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    // BrowserAnimationsModule,
-    // MdButtonModule, MdOptionModule, MdSelectModule
+    BrowserModule
   ],
   declarations: [
     AppComponent
